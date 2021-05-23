1b168340e9c184a533d511bd9ea03a3f46048769
Rocío Garrido <rocioganon@gmail.com>
2021-05-23 12:17:29 +0200

    add changes, but still don't work

--- js/main.js
+++ js/main.js
@@ -7,10 +7,10 @@ let favorite = []; // array para almacenar las series favoritas
 // Call to API. Obtener los datos de la API y guardarlos en JSON
 const search = document.querySelector(".js-search");
 
-function getSeries() {
+function getFromApi() {
   const inputValue = search.value;
   console.log(inputValue);
-  fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`)
+  fetch(`https://api.tvmaze.com/search/shows?q=${inputValue}`) // para construir la URL de búsqueda hay que recoger el texto que ha introducido la usuaria en el campo de búsqueda
     .then((response) => response.json()) // Le pido en el primer then que devuelva la respuesta en json
     .then((data) => {
       series = data; // guardo los datos en el array de series
@@ -18,42 +18,31 @@ function getSeries() {
     });
 }
 
-// Evitar el envío por defecto del input del formulario
-function handleForm(ev) {
-  ev.preventDefault();
-  getSeries();
-}
-search.addEventListener("submit", handleForm);
-
 // Pintar los datos de la búsqueda de la usuaria en el HTML (recorro el array)
 const listSeries = document.querySelector(".js-result");
 
 function paintSeries() {
   let htmlCode = ""; // creo una array vacía para la tarjeta
   for (const serie of series) {
-    let id = serie.show.id;
-    let name = serie.show.name;
-    let image = serie.show.image;
+    console.log(serie);
     // por cada show contenido en el resultado de la búsqueda hay que pintar una tarjeta donde mostramos:
-    htmlCode += `<li class="js-card serieCard" id="${id}">`; // el id de la serie
-    htmlCode += `<h3 class="js-name">${name}</h3>`; // muestra el nombre de la serie
-    if (image === null) {
-      htmlCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" alt="no image available"/>`; // imagen por defecto para aquellas series que no tienen
+    htmlCode += `<li class="serieCard" id="${serie.show.id}">`; // el id de la serie
+    htmlCode += `<h3>${serie.show.name}</h3>`; // muestra el nombre de la serie
+    if (serie.show.image === null) {
+      htmlCode += `<img src="https://via.placeholder.com/210x295/ffffff/666666/?text=TV" class= "serieImage" alt="no image available"/>`; // imagen por defecto para aquellas series que no tienen
     } else {
-      htmlCode += `<img src="${image.medium} class= "serieImage" alt="Image ${name}`; // muestra la imagen de la serie
+      htmlCode += `<img src="${serie.show.image.medium} class= "serieImage" alt="Image ${serie.show.name}`; // muestra la imagen de la serie
     }
     htmlCode += `</li>`;
   }
   listSeries.innerHTML = htmlCode;
-  return htmlCode;
 }
 
 // Evento listener al botón de búsqueda
-
 const btnSearch = document.querySelector(".js-button");
 
 function handleSearch() {
-  getSeries();
+  getFromApi();
 }
 
 btnSearch.addEventListener("click", handleSearch);
